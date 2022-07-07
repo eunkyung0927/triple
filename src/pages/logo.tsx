bef169174808325887fe928131bb481207e2c3d9
eun <tlsdmsrud927@naver.com>
2022-07-07 21:16:50 +0900

    [feat] logo 컴포넌트 추가 및 css적용

---
+++ src/pages/logo.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from 'styles/Logo.module.css'
+
+const Logo: NextPage = () => {
+  return (
+      <div className={styles.container}>
+       2019년 2월 기준
+     </div>
+  )
+}
+
+export default Logo
