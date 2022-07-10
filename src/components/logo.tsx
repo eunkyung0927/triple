622bb5c8eadc58b286be95db11419620ef7acccc
eun <tlsdmsrud927@naver.com>
2022-07-10 20:21:37 +0900

    [feat] logo 컴포넌트 추가 및 스타일 추가

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,9 @@
 import type { NextPage } from 'next'
+
 import styles from 'styles/Logo.module.css'
 
 const Logo: NextPage = () => {
-  return (
-      <div className={styles.container}>
-       2019년 2월 기준
-     </div>
-  )
+  return <div className={styles.container}>2019년 2월 기준</div>
 }
 
 export default Logo
