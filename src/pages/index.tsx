1598f3743da5890b3ef3e4e0e0582888e47cd211
eun <tlsdmsrud927@naver.com>
2022-07-10 22:37:22 +0900

    [chore] 사용하지 않는 css 파일제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,9 @@ import Logo from '../components/logo'
 import Content from '../components/content'
 import Awards from '../components/awards'
 
-import styles from 'styles/Home.module.css'
-
 const Home: NextPage = () => {
   return (
-    <div className={styles.wrapper}>
+    <div>
       <Logo />
       <Content />
       <Awards />
