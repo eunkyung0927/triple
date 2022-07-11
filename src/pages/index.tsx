529aa3984223862cbdca5f30660144ababce833b
eun <tlsdmsrud927@naver.com>
2022-07-11 19:51:19 +0900

    [feat] header부분 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 import Logo from '../components/logo'
 import Content from '../components/content'
@@ -7,6 +8,11 @@ import Awards from '../components/awards'
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title> Triple </title>
+        <link rel="icon" href="/triple2x.png" />
+        <meta property="og:title" content="triple" />
+      </Head>
       <Logo />
       <Content />
       <Awards />
